2d7de3bdabb2f4d941c1f2efc157575a2c10d647
Yug Solanki <yugsolanki15@gmail.com>
2023-12-02 00:19:50 +0530

    show dialog for issues, api for deleting issues

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -22,13 +22,33 @@ export async function PATCH(
     return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
 
   const updatedIssue = await prisma.issue.update({
-    where: { id: parseInt(params.id) },
+    where: { id: issue.id },
     data: {
       title: body.title,
       description: body.description,
-      status: body.status
+      status: body.status,
     },
   });
 
   return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  const deletedIssue = await prisma.issue.delete({
+    where: {
+      id: issue.id,
+    },
+  });
+
+  return NextResponse.json(deletedIssue);
 }
